17276500326d551ac6d14795cab3cf2382980e43
Tomikaze121 <jeanclaude.lequigan.22@usjr.edu.ph>
2025-05-20 05:02:47 +0800

    update ra gamay ambot lang hehe

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -4,7 +4,9 @@ import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Capacitor } from '@capacitor/core';
 import { Preferences } from '@capacitor/preferences';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root',
+})
 export class MusicService {
   private currentSound: Howl | null = null;
   private currentTrack: any = null;
@@ -12,53 +14,38 @@ export class MusicService {
   private currentIndex: number = 0;
 
   async play(track: any) {
-    if (!track) return;
+    if (!track || !track.path) return;
 
-    await Preferences.set({ key: 'currentTrack', value: JSON.stringify(track) });
+    this.stop();
+
     this.currentTrack = track;
+    await Preferences.set({ key: 'currentTrack', value: JSON.stringify(track) });
 
-    if (this.currentSound) {
-      this.currentSound.stop();
-      this.currentSound.unload();
-    }
-
-    let src = '';
-    try {
-      if (track.preview) {
-        src = track.preview;
-      } else if (track.path && track.path.startsWith('file://')) {
-        src = this.convertFileSrc(track.path);
-      } else if (track.path) {
-        const { uri } = await Filesystem.getUri({
-          path: track.path,
-          directory: Directory.Documents,
-        });
-        src = this.convertFileSrc(uri);
+    let source = track.path;
+    if (track.type === 'local') {
+      if (!source.startsWith('file://')) {
+        const { uri } = await Filesystem.getUri({ path: source, directory: Directory.Documents });
+        source = uri;
       }
-
-      this.currentSound = new Howl({
-        src: [src],
-        html5: true,
-        format: ['mp3', 'aac', 'wav', 'flac', 'ogg', 'opus'],
-        onend: () => this.playNext(),
-      });
-
-      this.currentSound.play();
-    } catch (error) {
-      console.error('Error playing track:', error);
+      source = this.convertFileSrc(source);
     }
+
+    this.currentSound = new Howl({
+      src: [source],
+      html5: true,
+      format: ['mp3', 'aac', 'wav', 'flac', 'ogg', 'opus'],
+      onend: () => this.playNext(),
+    });
+
+    this.currentSound.play();
   }
 
   pause() {
-    if (this.currentSound?.playing()) {
-      this.currentSound.pause();
-    }
+    if (this.currentSound?.playing()) this.currentSound.pause();
   }
 
   resume() {
-    if (this.currentSound && !this.currentSound.playing()) {
-      this.currentSound.play();
-    }
+    if (this.currentSound && !this.currentSound.playing()) this.currentSound.play();
   }
 
   stop() {
@@ -77,43 +64,46 @@ export class MusicService {
     return this.currentTrack;
   }
 
-  setStreamingTrack(track: any) {
-  this.playlist = [track];
-  this.currentIndex = 0;
-  this.play(track);
-}
+  getCurrentSound() {
+    return this.currentSound;
+  }
 
   async restoreTrack() {
     const { value } = await Preferences.get({ key: 'currentTrack' });
-    if (value) {
-      this.currentTrack = JSON.parse(value);
-    }
+    if (value) this.currentTrack = JSON.parse(value);
   }
 
-  setPlaylist(list: any[], startIndex: number) {
+  setPlaylist(list: any[], index: number) {
     this.playlist = list;
-    this.currentIndex = startIndex;
+    this.currentIndex = index;
     this.play(this.playlist[this.currentIndex]);
   }
 
   playNext() {
-    if (this.playlist.length > 0) {
+    if (this.playlist.length) {
       this.currentIndex = (this.currentIndex + 1) % this.playlist.length;
       this.play(this.playlist[this.currentIndex]);
     }
   }
 
   playPrevious() {
-    if (this.playlist.length > 0) {
+    if (this.playlist.length) {
       this.currentIndex = (this.currentIndex - 1 + this.playlist.length) % this.playlist.length;
       this.play(this.playlist[this.currentIndex]);
     }
   }
 
+  setStreamingTrack(track: any) {
+  this.playlist = [track];
+  this.currentIndex = 0;
+  this.play(track);
+}
+
+
   private convertFileSrc(uri: string): string {
-    if (Capacitor.getPlatform() === 'android') {
-      return uri.replace('file://', 'http://localhost/_capacitor_file_');
-    }
-    return uri;
+    return Capacitor.getPlatform() === 'android'
+      ? uri.replace('file://', 'http://localhost/_capacitor_file_')
+      : uri;
   }
+  
 }
